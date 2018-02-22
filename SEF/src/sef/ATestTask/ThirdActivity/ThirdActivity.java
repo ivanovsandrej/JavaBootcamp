ee3164513218fafcd563608b4ec0a4c6c8bf9ac7
SabineKuznecova <36644403+SabineKuznecova@users.noreply.github.com>
2018-02-22 20:34:23 +0200

    Add files via upload
    Change Third Activity

--- SEF/src/sef/ATestTask/ThirdActivity/ThirdActivity.java
+++ SEF/src/sef/ATestTask/ThirdActivity/ThirdActivity.java
@@ -9,35 +9,51 @@ public class ThirdActivity {
         ThirdActivity ta = new ThirdActivity();
         //TODO handle exception on proper way
         ta.validateUser("Ivan");
-        ta.catchExeption();
+        ta.catchExeption(0);
     }
 
-    void catchExeption() {
+    void catchExeption(int number) {
         int[] arr = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
-
-        for (int i = 0; i <= 10; i++) {
-            System.out.println(arr[i]);
+        try {
+            for (int i = 0; i <= 10; i++) {
+                System.out.println(arr[i]);
+            }
+        } catch (Exception e) {
+            System.out.println("This should get printed even if there is an exception");
         }
-        System.out.println("This should get printed even if there is an exception");
-
     }
 
     void validateUser(String name) {
         String[] validUsers = {"John", "Mike", "Missi", "Peacy"};
         int flag = 0;
+        for (int i = 0; i <= 3; i++) {
+            if (name.equals(validUsers[0])) {
+                flag = 1;
+            }
+        }
+
+        try {
+            if (flag == 0) new Exception();
+        } catch (Exception e) {
+            System.out.println("Generic Exception");
+        }
+
         //TODO if name in a list -> set flag=1
         // if at the end flag=0 -> throw the exeption
     }
-
-    void catchMe(int num1, int num2)
-    {
+    void catchMe(int num1, int num2) {
         //TODO Catch exeption
-            int result=num1/num2;
+        try {
+            int result = num1 / num2;
             System.out.println("The result is :" + result);
-
+        } catch (ArithmeticException ae) {
+            System.out.println("Arithmetic Exception");
+        } catch (Exception e) {
+            System.out.println("Generic Exception");
+        } finally {
             //TODO prints a message "Thank you for using this program." always
             System.out.println("Thank you for using this program.");
 
+        }
     }
 }
-
